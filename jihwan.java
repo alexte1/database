a9e11125e767d657f683963e3f1c04b17190eaf8
Ji Kim <jkim393@sledge.cs.ucr.edu>
2018-12-12 19:22:56 -0800

    AddCar and Initiate Service Request Done

--- jihwan.java
+++ jihwan.java
@@ -23,7 +23,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.ArrayList;
-
+import java.util.Random;
+import java.lang.Math;
 /**
  * This class defines a simple embedded SQL utility class that is designed to
  * work with PostgreSQL JDBC drivers.
@@ -351,37 +352,154 @@ public class MechanicShop{
 		}while (true);
 		return input;
 	}//end readChoice
-	
-	public static int getCID(MechanicShop esql){
-		try{
-				//String query = "SELECT COUNT(DISTINCT id) FROM Customer;";
-				String query = "SELECT * FROM Customer";
-			  int totalID = esql.executeQuery2(query);
-				System.out.println("Total number of distinct ids: " + totalID);
-				return totalID;//add 1 later
-		} catch(Exception e){
-				System.err.println(e.getMessage());
-				return -912833;
-		}
+	public static int getRandNum(){
+ 
+     int upperBound = (int) Math.pow(2, 32) - 1;
+     Random rand = new Random();
+     int n = (int) (Math.random() * upperBound);
+ 
+     return n;
 	}
+ 
+	public static int getCID(MechanicShop esql){
+	   try{
+	 
+	       boolean flag = true;
+	 
+	       while (flag) {
+	 
+	         String query = "Select id FROM Customer WHERE id = ";
+	 
+	         int randNum = getRandNum();
+	 
+	         query += randNum + ";";
+	 
+	         int rowCount = esql.executeQuery2(query);
+	 
+	         if(rowCount == 0){
+	           //System.out.println("Worked");
+	           //System.out.println(randNum);
+	           flag = false;
+	           return randNum;//add 1 later
+	         } else{
+	           //System.out.println("FAiled");
+	         }
+	       }
+	 
+       return -99999;
+ 
+         // return totalID;//add 1 later
+     } catch(Exception e){
+         System.err.println(e.getMessage());
+	         return -912833;
+	}
+ }
 
 	public static int getMID(MechanicShop esql){
-		try{
-				//String query = "SELECT COUNT(DISTINCT id) FROM Customer;";
-				String query = "SELECT * FROM Mechanic";
-			  int totalID = esql.executeQuery2(query);
-				System.out.println("Total number of distinct ids: " + totalID);
-				return totalID;//add 1 later
-		} catch(Exception e){
-				System.err.println(e.getMessage());
-				return -912833;
-		}
+	   try{
+	 
+	       boolean flag = true;
+	 
+	       while (flag) {
+	 
+	         String query = "Select id FROM Mechanic WHERE id = ";
+	 
+	         int randNum = getRandNum();
+	 
+	         query += randNum + ";";
+	 
+	         int rowCount = esql.executeQuery2(query);
+	 
+	         if(rowCount == 0){
+	           //System.out.println("Worked");
+	           //System.out.println(randNum);
+	           flag = false;
+	           return randNum;//add 1 later
+	         } else{
+	           //System.out.println("FAiled");
+	         }
+	       }
+	 
+       return -99999;
+ 
+         // return totalID;//add 1 later
+     } catch(Exception e){
+         System.err.println(e.getMessage());
+	         return -912833;
 	}
+ }
+	public static int getOID(MechanicShop esql){
+	   try{
+	 
+	       boolean flag = true;
+	 
+	       while (flag) {
+	 
+	         String query = "Select ownership_id FROM Owns WHERE ownership_id = ";
+	 
+	         int randNum = getRandNum();
+	 
+	         query += randNum + ";";
+	 
+	         int rowCount = esql.executeQuery2(query);
+	 
+	         if(rowCount == 0){
+	           //System.out.println("Worked");
+	           //System.out.println(randNum);
+	           flag = false;
+	           return randNum;//add 1 later
+	         } else{
+	           //System.out.println("FAiled");
+	         }
+	       }
+	 
+       return -99999;
+ 
+         // return totalID;//add 1 later
+     } catch(Exception e){
+         System.err.println(e.getMessage());
+	         return -912833;
+	}
+ }
+	public static int getRID(MechanicShop esql){
+	   try{
+	 
+	       boolean flag = true;
+	 
+	       while (flag) {
+	 
+	         String query = "Select rid FROM Service_Request WHERE rid = ";
+	 
+	         int randNum = getRandNum();
+	 
+	         query += randNum + ";";
+	 
+	         int rowCount = esql.executeQuery2(query);
+	 
+	         if(rowCount == 0){
+	          // System.out.println("Worked");
+	          // System.out.println(randNum);
+	           flag = false;
+	           return randNum;//add 1 later
+	         } else{
+	          // System.out.println("Failed");
+	         }
+	       }
+	 
+       return -99999;
+ 
+         // return totalID;//add 1 later
+     } catch(Exception e){
+         System.err.println(e.getMessage());
+	         return -912833;
+		}
+ 	}
 
 	public static void AddCustomer(MechanicShop esql){//1
 		try {
 				String query = "INSERT INTO Customer(id, fname, lname, phone, address) VALUES('";
-				query += getCID(esql) + "','";
+				int cid = getCID(esql);
+				query += cid + "','";
 
 				System.out.println("First Name: ");
 				String fname = in.readLine();
@@ -399,7 +517,14 @@ public class MechanicShop{
 				String address = in.readLine();
 				query += address + "');";
 
-				esql.executeQuery(query);
+				esql.executeUpdate(query); //updated Customer table
+				//display result
+				System.out.println("------------------------------------------------");
+				System.out.println("Success! New customer info shown below");
+				query = "SELECT * FROM Customer WHERE id=";
+				query+= cid + ";";
+				int x = esql.executeQueryAndPrintResult(query);
+				System.out.println("------------------------------------------------");
 		} catch (Exception e){
 				System.err.println(e.getMessage());
 		}
@@ -408,7 +533,8 @@ public class MechanicShop{
 	public static void AddMechanic(MechanicShop esql){//2
 		try {
 				String query = "INSERT INTO Mechanic(id, fname, lname, experience) VALUES('";
-				query += getMID(esql) + "','";
+				int mid = getMID(esql);
+				query += mid + "','";
 
 				System.out.println("First Name: ");
 				String fname = in.readLine();
@@ -422,48 +548,248 @@ public class MechanicShop{
 				String experience = in.readLine();
 				query += experience + "');";
 
-				esql.executeQuery(query);
+				esql.executeUpdate(query);
+				//display result
+				System.out.println("------------------------------------------------");
+				System.out.println("Success! New mechanic info shown below");
+				query = "SELECT * FROM Mechanic WHERE id=";
+				query+= mid + ";";
+				int x = esql.executeQueryAndPrintResult(query);
+				System.out.println("------------------------------------------------");
 		} catch (Exception e){
 				System.err.println(e.getMessage());
 		}
-			
 	}
 	
 	public static void AddCar(MechanicShop esql){//3
 		try {
-				String query = "INSERT INTO Car(vin, make, model, year) VALUES('";
+				
+				System.out.println("Enter customer_id for who owns the car: ");
+				String customerID = in.readLine();
+			
+				String query = "Select * FROM Customer WHERE id=";
+				query+= customerID + ";";
+				int customerExists = esql.executeQuery(query); //number of customers with this id
 
-				System.out.println("VIN: ");
-				String vin = in.readLine();
-				query += vin + "','";
+				if(customerExists!=0){ //id exists
+					System.out.println("Welcome Back! To add your car, please enter the following information");
 
-				System.out.println("Make: ");
-				String make = in.readLine();
-				query += make + "','";
+					query = "INSERT INTO Car(vin, make, model, year) VALUES('";
 
-				System.out.println("Model: ");
-				String model = in.readLine();
-				query += model + "','";
+					System.out.println("VIN: ");
+					String vin = in.readLine();
+					query += vin + "','";
 
-				System.out.println("Year: ");
-				String year = in.readLine();
-				query += year + "');";
+					System.out.println("Make: ");
+					String make = in.readLine();
+					query += make + "','";
 
-				esql.executeQuery(query);
+					System.out.println("Model: ");
+					String model = in.readLine();
+					query += model + "','";
+
+					System.out.println("Year: ");
+					String year = in.readLine();
+					query += year + "');";
+
+					esql.executeUpdate(query); //added to Car
+					
+					query = "INSERT INTO Owns(ownership_id, customer_id, car_vin) VALUES('";
+					int ownID = getOID(esql);
+					query += ownID + "', '";
+					query += customerID + "', '";
+					query += vin + "');";
+					esql.executeUpdate(query); //added to Owns
+					
+					//display result
+					System.out.println("------------------------------------------------");
+					System.out.println("Success! New car and ownership info shown below");
+					query = "SELECT * FROM Car WHERE vin='";
+					query+= vin + "';";
+					int x = esql.executeQueryAndPrintResult(query);
+					
+					System.out.println("------------------------------------------------");
+					query = "SELECT * FROM Owns WHERE ownership_id=";
+					query+= ownID + ";";
+					x = esql.executeQueryAndPrintResult(query);
+					System.out.println("------------------------------------------------");
+					
+				}
+				else { //id doesn't exist
+					 // customer doesn't exist; ask NEW Customer?
+					//ask if new customer
+					System.out.println("Wrong ID. Are you a new customer? [y/n]");
+					String answer = in.readLine();
+					boolean flag = true;
+					//String yes = "y"; String no = "n";
+					if (answer.equals("y")) {flag=false;}
+					if (answer.equals("n")) {flag=false;}
+					//if (flag){
+					//	System.out.println("answer was y");
+					//}
+					
+					//if (flag) {
+					while(flag){
+						System.out.println("Please enter [y/n]:");
+						answer = in.readLine();
+						if (answer.equals("y")) {flag=false;}
+						if (answer.equals("n")) {flag=false;}
+					}
+					//}
+					
+					if (answer.equals("y")){ //new customer
+						//add customer
+						//int result = AddCustomer(esql);		
+						System.out.println("-----------------------------------------");
+						System.out.println("Please add customer first");
+						System.out.println("-----------------------------------------");
+						return;
+					}
+					else { //not new customer
+						do { //keep rechecking customer id
+								//make sure said person is entering their own id.
+								//even though they forgot it		
+								System.out.println("Please enter correct customer id:");
+								customerID = in.readLine();
+								
+								query = "Select * FROM Customer WHERE id=";
+								query += customerID + ";";
+
+								customerExists = esql.executeQuery(query);
+								
+								} while(customerExists == 0);//end of while; finally got id
+						//insert
+						System.out.println("Welcome! To add your car, please enter the following information");
+
+						query = "INSERT INTO Car(vin, make, model, year) VALUES('";
+
+						System.out.println("VIN: ");
+						String vin = in.readLine();
+						query += vin + "','";
+
+						System.out.println("Make: ");
+						String make = in.readLine();
+						query += make + "','";
+
+						System.out.println("Model: ");
+						String model = in.readLine();
+						query += model + "','";
+
+						System.out.println("Year: ");
+						String year = in.readLine();
+						query += year + "');";
+
+						esql.executeUpdate(query); //added to Car
+					
+						query = "INSERT INTO Owns(ownership_id, customer_id, car_vin) VALUES('";
+						int ownID = getOID(esql);
+						query += ownID + "', '";
+						query += customerID + "', '";
+						query += vin + "');";
+						esql.executeUpdate(query); //added to Owns
+						//display result
+						
+						System.out.println("------------------------------------------------");
+						System.out.println("Success! New car and ownership info shown below");
+						query = "SELECT * FROM Car WHERE vin='";
+						query+= vin + "';";
+						int x = esql.executeQueryAndPrintResult(query);
+					
+						System.out.println("------------------------------------------------");
+						query = "SELECT * FROM Owns WHERE ownership_id=";
+						query+= ownID + ";";
+						x = esql.executeQueryAndPrintResult(query);
+						System.out.println("------------------------------------------------");
+						
+					}//end of id doesn't exist else
+				}
 		} catch (Exception e){
 				System.err.println(e.getMessage());
-		}
-		
-	}
+		}	//end of catch
+	}//end of addcar function
 	
 	public static void InsertServiceRequest(MechanicShop esql){//4
 		try{
-			//String query = ;
-		}
-		catch(Exception e){
-			System.err.println(e.getMessage());
-		}
-	}
+			int x = 0; //dummy integer for the result of executeQueryAndPrintResult
+			String customerID="";
+			String carID="";
+			String query = "SELECT * FROM Customer WHERE lname='";
+			System.out.println("Last Name: ");
+			String lname = in.readLine();
+			query += lname + "';";
+
+			int customerExists = esql.executeQuery(query); //return number of customers with this last name
+			if (customerExists != 0){ //if customer exists
+				System.out.println("---------------------------------------------------");	
+				x = esql.executeQueryAndPrintResult(query); //display the customer table with this lname
+				
+				//Prompt the user to choose her customer id; check if id is valid???
+				System.out.println("---------------------------------------------------");	
+				System.out.println("Choose your customer id: ");
+				customerID = in.readLine();
+			}
+			else{ //customer doesn't exist
+				System.out.println("No customer with that last name exists. Please follow the instruction below to add new customer");
+				AddCustomer(esql);
+				System.out.println("Input your customer id shown above: ");
+				customerID = in.readLine();
+			}	
+			
+			//find out how many cars owned by this customer
+			query = "SELECT car_vin FROM Owns WHERE customer_id='";
+			query += customerID + "';";
+				
+			int carExists = esql.executeQuery(query); //number of cars owned by the chosen customer
+				
+			if (carExists !=0){ //if this customer has cars to display 
+				System.out.println("---------------------------------------------------");	
+				x = esql.executeQueryAndPrintResult(query); //display cars(just car_vin) owned by the customer
+					//Prompt the user to choose her customer id;
+				System.out.println("---------------------------------------------------");
+				System.out.println("Choose your car_vin: ");
+				carID = in.readLine();
+			}
+			else { //this customer has no cars
+				System.out.println("This customer has no car. Please follow the instruction below to add new car");
+				AddCar(esql);
+				System.out.println("Input your car VIN(shown above): ");
+				carID = in.readLine();
+			}	
+			//verifies if this customer owns this car
+			query = "SELECT * FROM Owns WHERE car_vin='";
+			query += carID + "' AND customer_id='";
+			query += customerID + "';";
+				
+			int ownershipExists = esql.executeQuery(query);
+			if (ownershipExists != 0){
+				query = "INSERT INTO Service_Request(rid, customer_id, car_vin, date, odometer, complain) VALUES ('";
+				int rid = getRID(esql);
+				query += rid + "', '";
+				query += customerID + "', '" + carID + "', " + "CURRENT_DATE" + ", '";
+				System.out.println("Enter odometer:");
+				String odometer = in.readLine();
+				query += odometer + "', '";
+				System.out.println("Enter complain:");
+				String complain = in.readLine();
+				query += complain + "');";
+						
+				esql.executeUpdate(query);
+		
+				System.out.println("------------------------------------------------");
+				System.out.println("Success! New service request info shown below");
+				query = "SELECT * FROM Service_Request WHERE rid='";
+				query+= rid + "';";
+				x = esql.executeQueryAndPrintResult(query);
+				System.out.println("------------------------------------------------");
+	
+			}
+			else { //this customer doesn't own this car
+				System.out.println("this customer doesn't own this car");
+			}
+		} catch(Exception e){
+				System.err.println(e.getMessage());
+		}//end of catch
+	}//end of initiate service request
 	
 	public static void CloseServiceRequest(MechanicShop esql) throws Exception{//5
 		
@@ -472,7 +798,7 @@ public class MechanicShop{
 	public static void ListCustomersWithBillLessThan100(MechanicShop esql){//6
 		try{
 			String query = "SELECT date,comment,bill FROM Closed_Request WHERE bill < 100;";
-			int rowCount = esql.executeQuery3(query);
+			int rowCount = esql.executeQueryAndPrintResult(query);
 			System.out.println("total row(s): " + rowCount);
 		}
 	 	catch(Exception e){
@@ -483,7 +809,7 @@ public class MechanicShop{
 	public static void ListCustomersWithMoreThan20Cars(MechanicShop esql){//7
 		try{
 			String query = "SELECT fname,lname FROM Customer AS C, (SELECT customer_id, COUNT(customer_id) AS cnt FROM Owns GROUP BY customer_id HAVING COUNT(customer_id) > 20) AS N WHERE N.customer_id=C.id;";
-			int rowCount = esql.executeQuery3(query);
+			int rowCount = esql.executeQueryAndPrintResult(query);
 			System.out.println("total row(s): " + rowCount);
 		}
 	 	catch(Exception e){
@@ -494,7 +820,7 @@ public class MechanicShop{
 	public static void ListCarsBefore1995With50000Milles(MechanicShop esql){//8
 		try{
 			String query = "SELECT DISTINCT make, model, year FROM Car AS C, Service_Request AS S WHERE year < 1995 and S.car_vin = C.vin and S.odometer < 50000;";
-			int rowCount = esql.executeQuery3(query);
+			int rowCount = esql.executeQueryAndPrintResult(query);
 			System.out.println("total row(s): " + rowCount);
 		}
 	 	catch(Exception e){
@@ -510,7 +836,7 @@ public class MechanicShop{
 			String num = in.readLine();
 			query += num + ";";
 
-			int rowCount = esql.executeQuery3(query);
+			int rowCount = esql.executeQueryAndPrintResult(query);
 			System.out.println("total row(s): " + rowCount);
 		}
 	 	catch(Exception e){
@@ -521,7 +847,7 @@ public class MechanicShop{
 	public static void ListCustomersInDescendingOrderOfTheirTotalBill(MechanicShop esql){//9
 		try{
 			String query = "SELECT C.fname, C.lname, total FROM Customer AS C,(SELECT SR.customer_id, SUM(CR.bill) AS total FROM Closed_Request AS CR, Service_Request AS SR WHERE CR.rid = SR.rid GROUP BY SR.customer_id) AS B WHERE C.id=B.customer_id ORDER BY B.total DESC;";
-			int rowCount = esql.executeQuery3(query);
+			int rowCount = esql.executeQueryAndPrintResult(query);
 			System.out.println("total row(s): " + rowCount);
 		}
 	 	catch(Exception e){
